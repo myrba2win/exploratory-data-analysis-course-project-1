23c951b4077f0492bc4ab18ea10e149904772ad5
myrba2win <myrba2win@yahoo.com>
2020-08-26 05:50:35 -0400

    Uploading Files

---
+++ plot3.R
@@ -0,0 +1,23 @@
+# Read the base file "household_power_consumption.txt" that contains the data
+# Extract the filtered data according to the date range "2007-02-01" and "2007-02-02"
+# Plot a "Global Active Power" Histogram
+
+inputData <- "./data/household_power_consumption.txt"
+powerData <- read.table(inputData, header = TRUE, stringsAsFactors = FALSE, sep = ";")
+subData <- powerData[powerData$Date %in% c("1/2/2007","2/2/2007") ,]
+head(subData)
+
+
+#str(subData)
+datetime <- strptime(paste(subData$Date, subData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subData$Global_active_power)
+subMetering1 <- as.numeric(subData$Sub_metering_1)
+subMetering2 <- as.numeric(subData$Sub_metering_2)
+subMetering3 <- as.numeric(subData$Sub_metering_3)
+
+png("plot3.png", width=480, height=480)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
